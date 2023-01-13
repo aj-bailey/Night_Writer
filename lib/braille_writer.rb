8ad7ea4e9751b22224caa50bbf31c1aa7b13e720
Adam Bailey <ab67319@gmail.com>
2023-01-12 21:46:16 -0700

    refactor: update initialize for chars instance variable and attr_reader

--- lib/braille_writer.rb
+++ lib/braille_writer.rb
@@ -1,10 +1,14 @@
+require_relative "braille_char_generator"
+
 class BrailleWriter < File
   attr_reader :read_path, 
-              :write_path
+              :write_path,
+              :chars
 
   def initialize(argv)
     @read_path = argv[0]
     @write_path = argv[1]
+    @chars = BrailleCharGenerator.create_braille_characters('braille_characters.csv')
 
     p create_file
   end
