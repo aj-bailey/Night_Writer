3809c1ebbdffe45bdad5a89f3b2ce000b9198cf1
Adam Bailey <ab67319@gmail.com>
2023-01-12 18:31:07 -0700

    feat: create #create_file

--- lib/braille_writer.rb
+++ lib/braille_writer.rb
@@ -5,5 +5,11 @@ class BrailleWriter < File
   def initialize(argv)
     @read_path = argv[0]
     @write_path = argv[1]
+
+    p create_file
+  end
+
+  def create_file
+    "Created '#{@write_path}' containing #{open(@read_path).read.length} characters"
   end
 end
