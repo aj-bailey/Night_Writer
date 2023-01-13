98ba1ccaaa0d0f3ed2e28c8b0b02a890e27fd57c
Adam Bailey <ab67319@gmail.com>
2023-01-12 21:36:47 -0700

    feat: create braille_char_generator class file and class method

--- lib/braille_char_generator.rb
+++ lib/braille_char_generator.rb
@@ -1,5 +1,14 @@
 require 'csv'
+require_relative 'braille_char'
 
 class BrailleCharGenerator
+  def self.create_braille_characters(path)
+    braille_chars = []
 
+    CSV.foreach(path, headers: true, header_converters: :symbol ) do |info|
+      braille_chars << BrailleChar.new(info)
+    end
+
+    braille_chars
+  end
 end
