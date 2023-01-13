cdb7615a73fc4bb3ca578bdc1392f98ff113bd22
Adam Bailey <ab67319@gmail.com>
2023-01-12 21:23:20 -0700

    test: create examples 'has readable attributes'

--- spec/braille_char_spec.rb
+++ spec/braille_char_spec.rb
@@ -7,5 +7,21 @@ RSpec.describe BrailleChar do
     it 'exists' do
       expect(braille_char).to be_a(BrailleChar)
     end
+
+    it 'has readable letter' do
+      expect(braille_char.letter).to eq("a")
+    end
+
+    it 'has readable top row' do
+      expect(braille_char.top_row).to eq("0.")
+    end
+
+    it 'has readable middle row' do
+      expect(braille_char.middle_row).to eq("..")
+    end
+
+    it 'has readable bottom row' do
+      expect(braille_char.bottom_row).to eq("..")
+    end
   end
 end
