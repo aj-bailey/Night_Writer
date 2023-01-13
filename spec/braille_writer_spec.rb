c20b95a31ca8be12790cc249000f2eff4a5cdcf0
Adam Bailey <ab67319@gmail.com>
2023-01-12 17:56:36 -0700

    test: create '#initialize - exists' example

--- spec/braille_writer_spec.rb
+++ spec/braille_writer_spec.rb
@@ -1,6 +1,11 @@
 require_relative 'spec_helper'
 
 RSpec.describe BrailleWriter do
-  
+  let(:braille_writer) { BrailleWriter.new(["message.txt", "braille.txt"]) }
 
+  describe '#initialize' do
+    it 'exists' do
+      expect(braille_writer).to be_a(BrailleWriter)
+    end
+  end
 end
