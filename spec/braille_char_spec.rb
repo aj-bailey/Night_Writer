3aef99686492221f06232953139185b16f1776ec
Adam Bailey <ab67319@gmail.com>
2023-01-12 21:19:48 -0700

    test: create example '#initialize - exists'

--- spec/braille_char_spec.rb
+++ spec/braille_char_spec.rb
@@ -1,5 +1,11 @@
 require "braille_char"
 
 RSpec.describe BrailleChar do
+  let(:braille_char) { BrailleChar.new({letter: "a", top_row: "0.", middle_row: "..", bottom_row: ".."})}
 
+  describe '#initialize' do
+    it 'exists' do
+      expect(braille_char).to be_a(BrailleChar)
+    end
+  end
 end
