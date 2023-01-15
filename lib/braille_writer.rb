ef0a9c9fc36d7a1babc0606661dba0ae38cd0f39
Adam Bailey <ab67319@gmail.com>
2023-01-14 20:29:07 -0700

    fix: update gsub to bang method gsub

--- lib/braille_writer.rb
+++ lib/braille_writer.rb
@@ -27,7 +27,7 @@ class BrailleWriter
 
   def convert_text(text)
     chars_grouped_by_letter = @chars.group_by(&:letter)
-    # text = text.invalidate_text(text)
+    validated_text = invalidate_characters(text)
     braille_text = ""
     sets_of_40 = text.length / 40
 
@@ -35,16 +35,16 @@ class BrailleWriter
       starting_index = i * 40
       ending_index = (40 * (i + 1)) - 1
 
-      braille_text << text[starting_index..ending_index].chars.map { |letter| chars_grouped_by_letter[letter][0].top_row + " " }.join.concat("\n")
-      braille_text << text[starting_index..ending_index].chars.map { |letter| chars_grouped_by_letter[letter][0].middle_row + " " }.join.concat("\n")
-      braille_text << text[starting_index..ending_index].chars.map { |letter| chars_grouped_by_letter[letter][0].bottom_row + " " }.join.concat("\n\n")
+      braille_text << validated_text[starting_index..ending_index].chars.map { |letter| chars_grouped_by_letter[letter][0].top_row + " " }.join.concat("\n")
+      braille_text << validated_text[starting_index..ending_index].chars.map { |letter| chars_grouped_by_letter[letter][0].middle_row + " " }.join.concat("\n")
+      braille_text << validated_text[starting_index..ending_index].chars.map { |letter| chars_grouped_by_letter[letter][0].bottom_row + " " }.join.concat("\n\n")
     end
     
     braille_text
   end
 
   def invalidate_characters(text)
-    text.gsub("\n", " ")
+    text.gsub!("\n", " ")
     text.chars.reject { |character| !@chars.group_by(&:letter).include?(character) }.join
   end
 end
