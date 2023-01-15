cb42bc10b6b1eec0295723598f8288c34b18da64
Adam Bailey <ab67319@gmail.com>
2023-01-15 13:33:52 -0700

    test: create example '#read_file - can read a file to a string'

--- spec/character_writer_spec.rb
+++ spec/character_writer_spec.rb
@@ -1,11 +1,29 @@
 require_relative 'spec_helper'
 
 RSpec.describe CharacterWriter do
-  let(:character_writer) { CharacterWriter.new(["./spec/fixtures/test_input.txt", "./spec/fixtures/test_output.txt"])}
+  let(:character_writer) { CharacterWriter.new(["./spec/fixtures/test_char_input.txt", "./spec/fixtures/test_char_output.txt"])}
 
   describe '#initialize' do
     it 'exists' do
       expect(character_writer).to be_a(CharacterWriter)
+    end
+
+    it 'has readable read_path' do
+      expect(character_writer.read_path).to eq("./spec/fixtures/test_char_input.txt")
+    end
+
+    it 'has readable write_path' do
+      expect(character_writer.write_path).to eq("./spec/fixtures/test_char_output.txt")
+    end
+
+    it 'has readable chars' do
+      expect(character_writer.chars).to be_a(Array)
+    end
+  end
+
+  describe '#read_file' do
+    it 'can read a file to a string' do
+      expect(character_writer.read_file).to eq("test read")
     end
   end
 end
