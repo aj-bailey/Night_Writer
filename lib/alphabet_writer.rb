7a56e55da6440696bc627dc9673cccca034ded46
Adam Bailey <ab67319@gmail.com>
2023-01-14 21:57:13 -0700

    refactor: update initialize method and attr_reader

--- lib/alphabet_writer.rb
+++ lib/alphabet_writer.rb
@@ -1,5 +1,11 @@
 class AlphabetWriter
-  def initialize(argv)
+  attr_reader :read_path, 
+              :write_path,
+              :chars
 
+  def initialize(argv)
+    @read_path = argv[0]
+    @write_path = argv[1]
+    @chars = BrailleCharGenerator.create_braille_characters('braille_characters.csv')
   end
 end
