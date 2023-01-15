6ba4e46ff24b7e60d752e33cca267f2dfe382af3
Adam Bailey <ab67319@gmail.com>
2023-01-15 12:24:49 -0700

    refactor: update #convert_text to wrap after 40 characters

--- lib/alphabet_writer.rb
+++ lib/alphabet_writer.rb
@@ -26,6 +26,9 @@ class AlphabetWriter
   end
 
   def convert_text(text)
+    alphabet_text = ""
+    sets_of_three_braille_lines = [] 
+
     chars_group_by_braille_letter = @chars.group_by do |char|
       [char.top_row, char.middle_row, char.bottom_row]
     end.transform_values! { |char| char.first.letter }
@@ -35,8 +38,22 @@ class AlphabetWriter
     lines = validated_text.split("\n").map do |line|
       line.scan(/../)
     end
-    
-    lines.map.with_index { |line, index| chars_group_by_braille_letter[lines.transpose[index]] }.join    
+
+    (lines.length / 3).times do |index|
+      starting_index = index * 3
+      ending_index = (index * (3)) + 2
+
+      sets_of_three_braille_lines << lines[starting_index..ending_index]
+    end
+
+    sets_of_three_braille_lines.each do |set_of_three_braille_lines|
+      set_of_three_braille_lines.transpose.each do |braille_character|
+        alphabet_text << chars_group_by_braille_letter[braille_character]
+      end
+      alphabet_text << "\n"
+    end
+
+    alphabet_text.chomp
   end
 
   def invalidate_characters(text)
