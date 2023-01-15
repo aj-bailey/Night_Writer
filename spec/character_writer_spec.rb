afc296ec7b169f89a566463aa691f7a05d9863db
Adam Bailey <ab67319@gmail.com>
2023-01-15 13:22:46 -0700

    test: create example '#initialize - exists'

--- spec/character_writer_spec.rb
+++ spec/character_writer_spec.rb
@@ -1,6 +1,11 @@
 require_relative 'spec_helper'
 
 RSpec.describe CharacterWriter do
-  
+  let(:character_writer) { CharacterWriter.new(["./spec/fixtures/test_input.txt", "./spec/fixtures/test_output.txt"])}
 
+  describe '#initialize' do
+    it 'exists' do
+      expect(character_writer).to be_a(CharacterWriter)
+    end
+  end
 end
