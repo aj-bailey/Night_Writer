2aab5f3944eb261f7ed566a78b9f2f583bd07e8a
Adam Bailey <ab67319@gmail.com>
2023-01-15 13:34:58 -0700

    feat: create #read_file

--- lib/character_writer.rb
+++ lib/character_writer.rb
@@ -1,5 +1,17 @@
+require_relative "braille_char_generator"
+
 class CharacterWriter
+  attr_reader :read_path,
+              :write_path,
+              :chars
+
   def initialize(argv)
-    
+    @read_path = argv[0]
+    @write_path = argv[1]
+    @chars = BrailleCharGenerator.create_braille_characters('braille_characters.csv')
+  end
+
+  def read_file
+    File.open(@read_path).read
   end
 end
