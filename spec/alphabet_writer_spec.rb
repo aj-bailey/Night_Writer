b127f6fbb3016744bab5b009027ed04644a63a24
Adam Bailey <ab67319@gmail.com>
2023-01-14 21:56:51 -0700

    test: create example '#initialize - has readable attributes'

--- spec/alphabet_writer_spec.rb
+++ spec/alphabet_writer_spec.rb
@@ -7,5 +7,17 @@ RSpec.describe AlphabetWriter do
     it 'exists' do
       expect(alphabet_writer).to be_a(AlphabetWriter)
     end
+
+    it 'has readable read_path' do
+      expect(alphabet_writer.read_path).to eq("./spec/fixtures/test_braille_input.txt")
+    end
+
+    it 'has readable write_path' do
+      expect(alphabet_writer.write_path).to eq("./spec/fixtures/test_braille_output.txt")
+    end
+
+    it 'has readable chars' do
+      expect(alphabet_writer.chars).to be_a(Array)
+    end
   end
 end
