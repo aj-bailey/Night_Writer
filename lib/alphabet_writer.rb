434c76e682891144380a54315e4f407efb637b58
Adam Bailey <ab67319@gmail.com>
2023-01-16 11:07:27 -0700

    refactor: create #braille_char_to_alphabetical helper method

--- lib/alphabet_writer.rb
+++ lib/alphabet_writer.rb
@@ -12,33 +12,22 @@ class AlphabetWriter < CharacterWriter
   end
 
   def convert_text(text)
-    alphabet_text = ""
-    sets_of_three_braille_lines = [] 
-
-    chars_group_by_braille_letter = @chars.group_by do |char|
-      [char.top_row, char.middle_row, char.bottom_row]
-    end.transform_values! { |char| char.first.letter }
-
     validated_text = invalidate_characters(text)
-
+    
     lines = validated_text.split("\n").map do |line|
       line.scan(/../)
     end
-
-    (lines.length / 3).times do |index|
-      starting_index = index * 3
-      ending_index = (index * (3)) + 2
-
-      sets_of_three_braille_lines << lines[starting_index..ending_index]
-    end
-
+    
+    sets_of_three_braille_lines = lines.each_slice(3).to_a
+    
+    alphabet_text = ""
     sets_of_three_braille_lines.each do |set_of_three_braille_lines|
       set_of_three_braille_lines.transpose.each do |braille_character|
-        alphabet_text << chars_group_by_braille_letter[braille_character]
+        alphabet_text << braille_char_to_alphabetical(braille_character)
       end
       alphabet_text << "\n"
     end
-
+    
     alphabet_text.chomp
   end
 
@@ -46,5 +35,13 @@ class AlphabetWriter < CharacterWriter
     text.delete!(" ")
     text.gsub!("\n\n", "\n")
     text
+  end
+
+  def braille_char_to_alphabetical(braille_character)
+    chars_group_by_braille_letter = @chars.group_by do |char|
+      [char.top_row, char.middle_row, char.bottom_row]
+    end.transform_values! { |char| char.first.letter }
+
+    chars_group_by_braille_letter[braille_character]
   end
 end
